297ef50e7b7163c9d81844b1d8778c6a5ca0b88a
DESKTOP-DU7KIFO\Jeevan Dsouza <jeevandza4@gmail.com>
2021-07-15 20:09:35 +0530

    root changed to /api

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ const client = new textToSpeech.TextToSpeechClient({
 
 
 //send the request
-app.get('/', (req,res) =>{
+app.get('/api', (req,res) =>{
   res.send('wer are live')
 }) ;
-app.post('/', express.json(), async (req,res) =>{
+app.post('/api', express.json(), async (req,res) =>{
     const sessionId = uuid.v4();
     const sessionClient = new dialogflow.SessionsClient({
         keyFilename: require("path").join('./noyalbot-p9ii-0dc8295201bc.json')
